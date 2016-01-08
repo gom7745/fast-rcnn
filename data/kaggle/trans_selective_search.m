d194d8cb47395e73f11a78194bdd05c75e7f4095
gom7745 <b00902027@ntu.edu.tw>
2016-01-09 00:27:33 +0800

    Make trans_selective_search as function

--- data/kaggle/trans_selective_search.m
+++ data/kaggle/trans_selective_search.m
@@ -1,10 +1,11 @@
-load('train.mat');
+function [] = trans_selective_search(type)
+load([type '.mat']);
 boxes_temp = all_boxes;
-fileNames = textread('data/ImageSets/train.txt', '%s');
+fileNames = textread(['data/ImageSets/', type, '.txt'], '%s');
 
 for i = 1:size(boxes_temp,2)
     boxes_temp = uint16(all_boxes{1,i});
     boxes = boxes_temp(:, [2 1 4 3]) - 1;
     
-    save(['train_bbox/', fileNames{i,1} '_boxes.mat'], 'boxes');
+    save([type, '_bbox/', fileNames{i,1} '_boxes.mat'], 'boxes');
 end
